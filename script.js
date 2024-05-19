a6333d095678cf1659acaecdae93358e559fb8a0
Cnotdher_10 <107135919+hung0310@users.noreply.github.com>
2024-05-19 14:43:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-fetchData();
+// ------GET---------
+fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then(console.log)
 
-async function fetchData() {
-    try {
-        const response = await fetch('https://dummyjson.com/products');
-         
-        const data = await response.json();
-        console.log(data);
-    } catch(error) {
-        console.log(error);
-    }
-}
+// -------POST-------
+fetch('https://dummyjson.com/products/add', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        title: 'Hello World',
+        description: 'Hello World',
+        price: 10,
+        discountPercentage: 10,
+    })
+})
+.then(res => res.json())
+.then(console.log);
+
+// --------PUT-------
+fetch('https://dummyjson.com/products/1', {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        title: 'Hello World'
+    })
+})
+.then(res => res.json())
+.then(console.log);
+
+// -------DELETE------
+fetch('https://dummyjson.com/products/1', {
+    method: 'DELETE',
+})
+.then(res => res.json())
+.then(console.log);
